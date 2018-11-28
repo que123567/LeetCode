f79cda4f401fe03610f79d5ed3478b5d4004579b
qiuzhonghao <vip9504@163.com>
2018-11-28 15:56:56 +0800

    update 832/905

---
+++ src/LeetCode905.java
@@ -0,0 +1,55 @@
+/**
+ * 905. Sort Array By Parity
+ * <p>
+ * Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
+ * <p>
+ * You may return any answer array that satisfies this condition.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [3,1,2,4]
+ * Output: [2,4,3,1]
+ * The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+ * <p>
+ * <p>
+ * Note:
+ * <p>
+ * 1 <= A.length <= 5000
+ * 0 <= A[i] <= 5000
+ */
+public class LeetCode905 {
+
+    public static int[] sortArrayByParity(int[] A) {
+        int i = 0, j = A.length - 1;
+        while (i < j) {
+            if (isEven(A[i])) {
+                i++;
+            }
+            if (!isEven(A[j])) {
+                j--;
+            }
+            if (i >= j) {
+                break;
+            }
+            int temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+        }
+        return A;
+    }
+
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    public static void main(String[] args) {
+        int[] res = new int[]{3, 1, 2, 4};
+        res = sortArrayByParity(res);
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(res[i] + " ");
+        }
+
+    }
+}
