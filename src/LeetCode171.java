6e0a3076e3b4023c15d8f4a645590f3bd14bbf8e
qiuzhonghao <vip9504@163.com>
2018-11-29 14:32:39 +0800

    update 437

--- src/LeetCode171.java
+++ src/LeetCode171.java
@@ -32,11 +32,16 @@ public class LeetCode171 {
      * @return
      */
     public static int titleToNumber(String s) {
-
-        return 0;
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            result = result * 26;
+            result += (s.charAt(i) - 'A' + 1);
+        }
+        return result;
     }
 
     public static void main(String[] args) {
+        System.out.println(titleToNumber("ZY"));
 
     }
 }
