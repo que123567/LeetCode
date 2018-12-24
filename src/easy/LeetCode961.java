24b8cf46c314b59b96b3d6bdd128da29221248dc
qiuzhonghao <vip9504@163.com>
2018-12-24 14:45:07 +0800

    update 961

---
+++ src/easy/LeetCode961.java
@@ -0,0 +1,71 @@
+package easy;
+
+/**
+ * 961. N-Repeated Element in Size 2N Array
+ * <p>
+ * In a array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times.
+ * <p>
+ * Return the element repeated N times.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [1,2,3,3]
+ * Output: 3
+ * Example 2:
+ * <p>
+ * Input: [2,1,2,5,3,2]
+ * Output: 2
+ * Example 3:
+ * <p>
+ * Input: [5,1,5,2,5,3,5,4]
+ * Output: 5
+ * <p>
+ * <p>
+ * Note:
+ * <p>
+ * 4 <= A.length <= 10000
+ * 0 <= A[i] < 10000
+ * A.length is even
+ */
+public class LeetCode961 {
+
+    /**
+     * 数组统计
+     *
+     * @param A
+     * @return
+     */
+    public static int repeatedNTimes(int[] A) {
+        int[] map = new int[10001];
+        for (int i : A) {
+            if (++map[i] > 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 随机数
+     *
+     * @param A
+     * @return
+     */
+    public static int repeatedNTimes_(int[] A) {
+        int i = 0;
+        int j = 0;
+        int length = A.length;
+        while (i == j || A[i] != A[j]) {
+            i = (int) (Math.random() * length);
+            j = (int) (Math.random() * length);
+        }
+        return A[i];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(repeatedNTimes_(new int[]{1, 2, 3, 3}));
+
+    }
+}
