38f3809cdc0e8f061fd9eb26b9703a9a4b5ba0ba
qiuzhonghao <vip9504@163.com>
2018-12-24 16:22:57 +0800

    update 961/763

--- src/easy/LeetCode961.java
+++ src/easy/LeetCode961.java
@@ -2,29 +2,29 @@ package easy;
 
 /**
  * 961. N-Repeated Element in Size 2N Array
- * <p>
+ * .
  * In a array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times.
- * <p>
+ * .
  * Return the element repeated N times.
- * <p>
- * <p>
- * <p>
+ * .
+ * .
+ * .
  * Example 1:
- * <p>
+ * .
  * Input: [1,2,3,3]
  * Output: 3
  * Example 2:
- * <p>
+ * .
  * Input: [2,1,2,5,3,2]
  * Output: 2
  * Example 3:
- * <p>
+ * .
  * Input: [5,1,5,2,5,3,5,4]
  * Output: 5
- * <p>
- * <p>
+ * .
+ * .
  * Note:
- * <p>
+ * .
  * 4 <= A.length <= 10000
  * 0 <= A[i] < 10000
  * A.length is even
