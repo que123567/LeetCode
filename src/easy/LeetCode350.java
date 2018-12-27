80c184ad22fd692d5e2994165ce8f639f3d9bf9b
qiuzhonghao <vip9504@163.com>
2018-12-27 21:47:11 +0800

    update 350

--- src/easy/LeetCode350.java
+++ src/easy/LeetCode350.java
@@ -1,34 +1,54 @@
 package easy;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 350. Intersection of Two Arrays II
  * Given two arrays, write a function to compute their intersection.
- *
+ * .
  * Example 1:
- *
+ * .
  * Input: nums1 = [1,2,2,1], nums2 = [2,2]
  * Output: [2,2]
  * Example 2:
- *
+ * .
  * Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
  * Output: [4,9]
  * Note:
- *
+ * .
  * Each element in the result should appear as many times as it shows in both arrays.
  * The result can be in any order.
  * Follow up:
- *
+ * .
  * What if the given array is already sorted? How would you optimize your algorithm?
  * What if nums1's size is small compared to nums2's size? Which algorithm is better?
  * What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
- *
  */
 public class LeetCode350 {
     public static int[] intersect(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> intersect = new HashSet<>();
 
+        for (int i : nums1) {
+            set.add(i);
+        }
+        for (int i : nums2) {
+            if (set.contains(i)) {
+                intersect.add(i);
+            }
+        }
+        int[] res = new int[intersect.size()];
+        int i = 0;
+        for (int num : intersect) {
+            res[i++] = num;
+        }
+        return res;
     }
 
     public static void main(String[] args) {
+        int[] res = intersect(new int[]{1, 2, 2, 1}, new int[]{2, 2});
+        System.out.println(res[0]);
 
     }
 }
