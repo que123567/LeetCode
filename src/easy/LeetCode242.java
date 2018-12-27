88b3faf9d14f59eb9c11c8bce6c1c48d6a19e01b
qiuzhonghao <vip9504@163.com>
2018-12-27 16:51:04 +0800

    update 167/844

--- src/easy/LeetCode242.java
+++ src/easy/LeetCode242.java
@@ -54,7 +54,6 @@ public class LeetCode242 {
      * @return
      */
     public static boolean isAnagram_(String s, String t) {
-
         int[] alphabet = new int[26];
         for (int i = 0; i < s.length(); i++)alphabet[s.charAt(i) - 'a']++;
         for (int i = 0; i < t.length(); i++)alphabet[t.charAt(i) - 'a']--;
