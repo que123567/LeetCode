4d6f54cc88d6462657fa270ad07adc9d52385ebc
qiuzhonghao <vip9504@163.com>
2018-12-28 09:33:48 +0800

    update 350

--- src/easy/LeetCode350.java
+++ src/easy/LeetCode350.java
@@ -1,7 +1,10 @@
 package easy;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 /**
  * 350. Intersection of Two Arrays II
@@ -27,28 +30,26 @@ import java.util.Set;
  */
 public class LeetCode350 {
     public static int[] intersect(int[] nums1, int[] nums2) {
-        Set<Integer> set = new HashSet<>();
-        Set<Integer> intersect = new HashSet<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
 
-        for (int i : nums1) {
-            set.add(i);
+        for (int i = 0; i < nums1.length; i++) {
+            map.put(nums1[i], map.getOrDefault(nums1[i], 0) + 1);
         }
-        for (int i : nums2) {
-            if (set.contains(i)) {
-                intersect.add(i);
+        for (int i = 0; i < nums2.length; i++) {
+            if (map.containsKey(nums2[i]) && map.get(nums2[i]) > 0) {
+                list.add(nums2[i]);
+                map.put(nums2[i], map.get(nums2[i]) - 1);
             }
         }
-        int[] res = new int[intersect.size()];
-        int i = 0;
-        for (int num : intersect) {
-            res[i++] = num;
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
         }
         return res;
     }
 
     public static void main(String[] args) {
         int[] res = intersect(new int[]{1, 2, 2, 1}, new int[]{2, 2});
-        System.out.println(res[0]);
-
     }
 }
