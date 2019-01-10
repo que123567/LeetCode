e6268e2050f13754cda8acb71101cf99f58c3214
qiuzhonghao <vip9504@163.com>
2019-01-10 14:43:44 +0800

    update 200

--- src/easy/LeetCode118.java
+++ src/easy/LeetCode118.java
@@ -16,10 +16,10 @@ import java.util.List;
  * Input: 5
  * Output:
  * [
- *     [1],
- *    [1,1],
- *   [1,2,1],
- *  [1,3,3,1],
+ * [1],
+ * [1,1],
+ * [1,2,1],
+ * [1,3,3,1],
  * [1,4,6,4,1]
  * ]
  */
@@ -30,7 +30,7 @@ public class LeetCode118 {
         List<Integer> row = new ArrayList<>();
         for (int i = 0; i < numRows; i++) {
             row.add(0, 1);
-            for (int j = 1; j <row.size()-1 ; j++) {
+            for (int j = 1; j < row.size() - 1; j++) {
                 row.set(j, row.get(j) + row.get(j + 1));
             }
             allrows.add(new ArrayList<>(row));
--- src/easy/LeetCode897.java
+++ src/easy/LeetCode897.java
@@ -4,12 +4,12 @@ import DataStructure.TreeNode;
 
 /**
  * 897. Increasing Order Search Tree
- * <p>
+ * .
  * Given a tree, rearrange the tree in in-order so that the leftmost node in the tree is now the root of the tree, and every node has no left child and only 1 right child.
- * <p>
+ * .
  * Example 1:
  * Input: [5,3,6,2,4,null,8,1,null,null,null,7,9]
- * <p>
+ * .
  * 5
  * /   \
  * 3     6
@@ -17,9 +17,9 @@ import DataStructure.TreeNode;
  * 2   4     8
  * /         / \
  * 1         7   9
- * <p>
+ * .
  * Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
- * <p>
+ * .
  * 1
  * \
  * 2
