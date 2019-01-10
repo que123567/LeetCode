fd9dc77b7ea39ff998f4361fc459486245ed79d8
qiuzhonghao <vip9504@163.com>
2019-01-10 16:59:16 +0800

    update

--- src/medium/LeetCode200.java
+++ src/medium/LeetCode200.java
@@ -26,15 +26,15 @@ package medium;
 public class LeetCode200 {
     /**
      * The algorithm works as follow:
-     *
+     * '
      * Scan each cell in the grid.
      * If the cell value is '1' explore that island.
      * Mark the explored island cells with 'x'.
      * Once finished exploring that island, increment islands counter.
      * The arrays dx[], dy[] store the possible moves from the current cell. Two land cells ['1'] are considered from the same island if they are horizontally or vertically adjacent (possible moves (-1,0),(0,1),(0,-1),(1,0)). Two '1' diagonally adjacent are not considered from the same island.
      */
-    static int[] dx = {-1,0,0,1};
-    static int[] dy = {0,1,-1,0};
+    static int[] dx = {-1, 0, 0, 1};
+    static int[] dy = {0, 1, -1, 0};
 
     public static int numIslands(char[][] grid) {
         if (grid == null || grid.length == 0) {
@@ -55,12 +55,11 @@ public class LeetCode200 {
     private static void explore(char[][] grid, int i, int j) {
         grid[i][j] = '-';
         for (int d = 0; d < 4; d++) {
-            if (i + dy[d] < grid.length && i + dy[d] >= 0 && j + dx[d] < grid[0].length && j + dx[d] >= 0 && grid[i+dy[d]][j+dx[d]] == '1') {
+            if (i + dy[d] < grid.length && i + dy[d] >= 0 && j + dx[d] < grid[0].length && j + dx[d] >= 0 && grid[i + dy[d]][j + dx[d]] == '1') {
                 explore(grid, i + dy[d], j + dx[d]);
             }
         }
     }
-
 
 
     public static void main(String[] args) {
