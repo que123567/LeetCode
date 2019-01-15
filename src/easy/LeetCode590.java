45581bbaf91bec247a172a36856df0d26a4bdf60
qiuzhonghao <vip9504@163.com>
2019-01-15 11:22:21 +0800

    update 590

--- src/easy/LeetCode590.java
+++ src/easy/LeetCode590.java
@@ -66,28 +66,6 @@ public class LeetCode590 {
         return list;
     }
 
-    /**
-     * version3
-     */
-    public static class Solution {
-        TreeNode prev = null, head = null;
-
-        public TreeNode increasingBST(TreeNode root) {
-            if (root == null)
-                return null;
-            increasingBST(root.left);
-            if (prev != null) {
-                root.left = null; // we no  longer needs the left  side of the node, so set it to null
-                prev.right = root;
-            }
-            if (head == null)
-                head = root; // record the most left node as it will be our root
-            prev = root; //keep track of the prev node
-            increasingBST(root.right);
-            return head;
-        }
-    }
-
     public static void main(String[] args) {
         Node node = new Node();
         node.val = 1;
