cd3b19259e01e18b0aacd8c4c124a5a3bf07ec9d
qiuzhonghao <vip9504@163.com>
2019-01-23 15:35:17 +0800

    update 696

---
+++ src/easy/LeetCode696.java
@@ -0,0 +1,61 @@
+package easy;
+
+/**
+ * 696. Count Binary Substrings
+ * .
+ * Give a string s, count the number of non-empty (contiguous) substrings that have the same number of 0's and 1's, and all the 0's and all the 1's in these substrings are grouped consecutively.
+ * .
+ * Substrings that occur multiple times are counted the number of times they occur.
+ * .
+ * Example 1:
+ * Input: "00110011"
+ * Output: 6
+ * Explanation: There are 6 substrings that have equal number of consecutive 1's and 0's: "0011", "01", "1100", "10", "0011", and "01".
+ * .
+ * Notice that some of these substrings repeat and are counted the number of times they occur.
+ * .
+ * Also, "00110011" is not a valid substring because all the 0's (and 1's) are not grouped together.
+ * Example 2:
+ * Input: "10101"
+ * Output: 4
+ * Explanation: There are 4 substrings: "10", "01", "10", "01" that have equal number of consecutive 1's and 0's.
+ * Note:
+ * .
+ * s.length will be between 1 and 50,000.
+ * s will only consist of "0" or "1" characters.
+ */
+public class LeetCode696 {
+    public static int countBinarySubstrings(String s) {
+        int cur = 1, pre = 0, res = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i - 1) == s.charAt(i)) {
+                cur++;
+            } else {
+                res += Math.min(pre, cur);
+                pre = cur;
+                cur = 1;
+            }
+        }
+        return res + Math.min(pre, cur);
+    }
+
+    public static int countBinarySubstrings_v2(String s) {
+        int prevRunLength = 0, curRunLength = 1, res = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1))
+                curRunLength++;
+            else {
+                prevRunLength = curRunLength;
+                curRunLength = 1;
+            }
+            if (prevRunLength >= curRunLength)
+                res++;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.err.println(countBinarySubstrings("00110011"));
+    }
+
+}
