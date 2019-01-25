eb52d50c574462f27d6443ee202ba5483df623b5
qiuzhonghao <vip9504@163.com>
2019-01-25 11:12:26 +0800

    update 725

--- src/DataStructure/ListNode.java
+++ src/DataStructure/ListNode.java
@@ -38,10 +38,10 @@ public class ListNode {
 
     public static ListNode makeListNodeTestCase5() {
         ListNode node = new ListNode(1);
-        node.next = new ListNode(3);
-        node.next.next = new ListNode(2);
-        node.next.next.next = new ListNode(3);
-        node.next.next.next.next = new ListNode(1);
+        node.next = new ListNode(2);
+        node.next.next = new ListNode(3);
+        node.next.next.next = new ListNode(4);
+        node.next.next.next.next = new ListNode(5);
         return node;
     }
 }
