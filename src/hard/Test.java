ae161db96aa874d88298c28f6e3cfba3b93d91ed
qiuzhonghao <vip9504@163.com>
2019-03-07 16:16:49 +0800

    update 985(复习老代码)

--- src/hard/Test.java
+++ src/hard/Test.java
@@ -1,23 +1,9 @@
 package hard;
 
+import java.io.FileInputStream;
+
 public class Test {
-    public static int count_one(int n) {
-        int count = 0;
-        while (n != 0) {
-            n = n & (n - 1);
-            count++;
-        }
-        return count;
-    }
-
-
-    public static int getSum(int a, int b) {
-        return b == 0 ? a : getSum(a ^ b, (a & b) << 1); //be careful about the terminating condition;
-    }
-
+     ///Users/Captain/Desktop/grade_video_backup.mp4
     public static void main(String[] args) {
-        System.out.println(getSum(10, 20));
-        System.out.println(~2);
-
     }
 }
