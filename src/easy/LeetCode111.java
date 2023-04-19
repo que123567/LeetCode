8a320a94d74d858f23b5aa308dd5c32bfdf147bb
qiuzhonghao <qiuzhonghao@star-net.cn>
2023-04-19 22:02:23 +0800

    1.update 111

--- src/easy/LeetCode111.java
+++ src/easy/LeetCode111.java
@@ -13,23 +13,26 @@ public class LeetCode111 {
         if (root == null) {
             return min;
         }
+
+        //下面2个if的条件容易漏，如果是单叉树不加下面2个if判断直接返回depth为1（root）
         if (root.left == null) {
             return minDepth(root.right, min + 1);
         }
         if (root.right == null) {
             return minDepth(root.left, min + 1);
         }
+
         return Math.min(minDepth(root.left, min + 1), minDepth(root.right, min + 1));
     }
 
     public static void main(String[] args) {
-        TreeNode treeNode = new TreeNode(5);
-        treeNode.right = new TreeNode(13);
-        treeNode.right.left = new TreeNode(13);
-        treeNode.right.right = new TreeNode(13);
+        TreeNode treeNode1 = new TreeNode(2);
+        treeNode1.right = new TreeNode(3);
+        treeNode1.right.right = new TreeNode(4);
+        treeNode1.right.right.right = new TreeNode(5);
+        treeNode1.right.right.right.right = new TreeNode(6);
 
-        int a = minDepth(treeNode);
+        int a = minDepth(treeNode1);
         System.out.println(a);
-
     }
 }
