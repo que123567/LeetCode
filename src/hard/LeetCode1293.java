9135e436dffe1d3507fd0bb475f8a0786e777ed2
qiuzhonghao <qiuzhonghao@star-net.cn>
2023-04-20 22:24:33 +0800

    1.add code

---
+++ src/hard/LeetCode1293.java
@@ -0,0 +1,17 @@
+package hard;
+
+/**
+ * 1293. 网格中的最短路径 https://leetcode.cn/problems/shortest-path-in-a-grid-with-obstacles-elimination/description/
+ */
+public class LeetCode1293 {
+
+    static class Solution {
+        public int shortestPath(int[][] grid, int k) {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
