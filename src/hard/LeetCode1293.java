336928cbd22121ddf47567ce1006cc0ae016028b
qiuzhonghao <qiuzhonghao@star-net.cn>
2023-04-20 22:30:34 +0800

    1.add code

--- src/hard/LeetCode1293.java
+++ src/hard/LeetCode1293.java
@@ -2,6 +2,10 @@ package hard;
 
 /**
  * 1293. 网格中的最短路径 https://leetcode.cn/problems/shortest-path-in-a-grid-with-obstacles-elimination/description/
+ *
+ * https://leetcode.cn/problems/shortest-path-in-a-grid-with-obstacles-elimination/solutions/232460/wang-ge-zhong-de-zui-duan-lu-jing-bfssuan-fa-shi-x/
+ *
+ * https://leetcode.cn/problems/shortest-path-in-a-grid-with-obstacles-elimination/solutions/1740488/tu-jie-by-zhug-4-cst1/
  */
 public class LeetCode1293 {
 
